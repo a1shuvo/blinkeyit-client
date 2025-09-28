8addf0941164a6b2251b94c1fa9518ba222fb357
a1shuvo <a1shuvosaha@gmail.com>
2025-09-28 20:34:14 +0600

    Added profile image upload and display in user dashboard

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import { FaRegUserCircle } from "react-icons/fa";
 import { useSelector } from "react-redux";
+import UserProfileAvatarEdit from "../components/UserProfileAvatarEdit";
 
 const Profile = () => {
   const user = useSelector((state) => state?.user);
-  console.log("profile", user);
+  const [profileAvatarEdit, setProfileAvatarEdit] = useState(false);
 
   return (
     <div>
+      {/* Profile image upload and display */}
       <div className="w-20 h-20 flex items-center justify-center rounded-full overflow-hidden">
         {user.avatar ? (
           <img src={user.avatar} alt={user.name} className="w-full h-full" />
@@ -14,9 +17,16 @@ const Profile = () => {
           <FaRegUserCircle size={65} />
         )}
       </div>
-      <button className="text-sm min-w-20 border border-primary-100 hover:border-primary-200 hover:bg-primary-200 px-3 py-1 mt-3 rounded-full cursor-pointer">
+      <button
+        onClick={() => setProfileAvatarEdit(true)}
+        className="text-sm min-w-20 border border-primary-100 hover:border-primary-200 hover:bg-primary-200 px-3 py-1 mt-3 rounded-full cursor-pointer"
+      >
         Edit
       </button>
+
+      {profileAvatarEdit && (
+        <UserProfileAvatarEdit close={() => setProfileAvatarEdit(false)} />
+      )}
     </div>
   );
 };
