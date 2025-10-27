214ab4bbc9a21eb7aa144487745e2eb80bdfac23
a1shuvo <a1shuvosaha@gmail.com>
2025-10-27 19:57:30 +0600

    Added upload product page with name and description input field

--- src/pages/UploadProduct.jsx
+++ src/pages/UploadProduct.jsx
@@ -1,10 +1,63 @@
-import React from 'react';
-
+import { useState } from "react";
 const UploadProduct = () => {
+  const [data, setData] = useState({
+    name: "",
+    image: [],
+    category: [],
+    subCategory: [],
+    unit: [],
+    stock: "",
+    price: "",
+    discount: "",
+    description: "",
+    more_details: {},
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
   return (
-    <div>
-      UploadProduct
-    </div>
+    <section>
+      <div className="p-2 bg-white shadow-md flex items-center justify-between">
+        <h2 className="font-semibold">Upload Product</h2>
+      </div>
+      <div className="grid p-3">
+        <form className="grid gap-2">
+          <div className="grid gap-1">
+            <label htmlFor="name">Name</label>
+            <input
+              id="name"
+              type="text"
+              placeholder="Enter product name"
+              name="name"
+              value={data.name}
+              onChange={handleChange}
+              className="bg-blue-50 p-2 border border-blue-100 focus-within:border-primary-200 outline-none rounded"
+              required
+            />
+          </div>
+          <div className="grid gap-1">
+            <label htmlFor="description">Description</label>
+            <textarea
+              id="description"
+              rows={3}
+              placeholder="Enter product description"
+              name="description"
+              value={data.description}
+              onChange={handleChange}
+              className="bg-blue-50 p-2 border border-blue-100 focus-within:border-primary-200 outline-none rounded resize-none"
+              required
+            ></textarea>
+          </div>
+        </form>
+      </div>
+    </section>
   );
 };
 
